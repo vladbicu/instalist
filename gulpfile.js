c6b02bf7eacd78f8a769d306dd45c01b3835aeeb
Lucian Samson <lucian.samson@cynny.com>
2015-12-18 15:26:14 +0200

    wip workflow

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+/* eslint-disable no-var */
+/* eslint-disable prefer-arrow-callback */
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var lessReporter = require('gulp-less-reporter');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+
+var browserify = require('browserify');
+var watchify = require('watchify');
+var LessPluginCleanCSS = require('less-plugin-clean-css');
+var sequence = require('run-sequence');
+
+var cleancss = new LessPluginCleanCSS({'advanced': true});
+
+var handleCompileError = function compileError(err) {
+	console.log('\n\tComplile failed due to browserify error:\n\t' + err.message + '\n');
+	/* eslint-disable no-invalid-this */
+	this.emit('end');
+	/* eslint-enable no-invalid-this */
+};
+
+var watchBundlerTask = function watchBundlerTask(filep, filen) {
+	// Enable source maps that allow you to debug your files separately.
+	var args = Object.assign({}, {
+		entries: [filep],
+		cache: {},
+		packageCache: {},
+		plugin: [watchify],
+		debug: true
+	});
+
+	var watchBundler = browserify(args);
+
+	return watchBundler
+			.bundle()
+			.on('error', handleCompileError)
+			.pipe(source(filen))
+			.pipe(buffer())
+			.pipe(gulp.dest('./bin/js'));
+};
+
+
+gulp.task('less', function compileLess() {
+	return gulp.src('./lib/css/style.less')
+		.pipe(less({'plugins': [cleancss]}).on('error', lessReporter))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('bin/css/'));
+});
+
+gulp.task('copy', function cp(){
+	gulp.src('./lib/*.html').pipe(gulp.dest('./bin'));
+	gulp.src('./lib/css/fonts/proxima/*').pipe(gulp.dest('./bin/css/fonts/proxima'));
+	gulp.src('./lib/img/*').pipe(gulp.dest('./bin/img'));
+	gulp.src('./lib/js/onepagescroll.js').pipe(gulp.dest('./bin/js'));
+	gulp.src('./lib/js/main.js').pipe(gulp.dest('./bin/js'));
+});
+
+gulp.task('eslint', function lint() {
+	return gulp.src('./lib/js/**/*.js')
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failOnError());
+});
+
+gulp.task('watch:less', function () {
+
+	gulp.watch('./lib/css/**/*.less', function () {
+		console.log('*** Building LESS ***');
+		return sequence('less');
+	});
+
+});
+
+gulp.task('watch:js', function () {
+	gulp.watch('./lib/js/**/*.js', function() {
+		console.log('*** Building JS ***');
+		return watchBundlerTask('./lib/js/main.js', 'main.js');
+	});
+});
+
+gulp.task('default', function(){
+	return sequence(
+		'copy',
+		'watch:less',
+		'eslint',
+		'watch:js'
+	);
+});
